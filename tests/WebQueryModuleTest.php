0f9b0005bf276198caf02a6121695209b85fbc4f
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-04-24 16:20:11 +0900

    Bump phpstan version

--- tests/WebQueryModuleTest.php
+++ tests/WebQueryModuleTest.php
@@ -9,6 +9,9 @@ use Ray\Di\AbstractModule;
 use Ray\Di\Injector;
 use Ray\MediaQuery\WebApi\FooItemInterface;
 
+use function assert;
+use function is_callable;
+
 class WebQueryModuleTest extends TestCase
 {
     /** @var AbstractModule */
@@ -26,12 +29,15 @@ class WebQueryModuleTest extends TestCase
         $mediaQueryJson = __DIR__ . '/Fake/web_query.json';
         $module = new MediaQueryModule($mediaQueries, [new WebQueryConfig($mediaQueryJson, ['domain' => 'httpbin.org'])]);
         $this->injector = new Injector($module);
-        $this->logger = $this->injector->getInstance(MediaQueryLoggerInterface::class);
+        $logger = $this->injector->getInstance(MediaQueryLoggerInterface::class);
+        assert($logger instanceof MediaQueryLoggerInterface);
+        $this->logger = $logger;
     }
 
     public function testGetRequest(): void
     {
         $fooItem = $this->injector->getInstance(FooItemInterface::class);
+        assert(is_callable($fooItem));
         $response = ($fooItem)('hello');
         $this->assertSame('https://httpbin.org/anything/hello', $response['url']);
         $this->assertSame('query: https://httpbin.org/anything/hello({"id":"hello"})', (string) $this->logger);
