c88e614807fe29973926a854d7c49a690acbae1e
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-11-21 21:26:29 +0900

    Revert annotations

--- src/Annotation/Pager.php
+++ src/Annotation/Pager.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace Ray\MediaQuery\Annotation;
 
 use Attribute;
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 
+/**
+ * @Annotation
+ * @Target("METHOD")
+ * @NamedArgumentConstructor
+ */
 #[Attribute(Attribute::TARGET_METHOD)]
 final class Pager
 {
