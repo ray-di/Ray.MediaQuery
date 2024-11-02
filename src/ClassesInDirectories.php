427290a83ba9e5c9a3573a8103e956b2357c7585
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-03 00:58:31 +0900

    Refactor class and namespace extraction
    Separated class and namespace extraction into distinct methods, improving readability and maintainability. Added assertions to ensure token types and values during extraction.

--- src/ClassesInDirectories.php
+++ src/ClassesInDirectories.php
@@ -14,6 +14,8 @@ use function class_exists;
 use function count;
 use function file_get_contents;
 use function interface_exists;
+use function is_array;
+use function is_string;
 use function token_get_all;
 
 use const T_CLASS;
@@ -69,41 +71,65 @@ final class ClassesInDirectories
             return null;
         }
 
-        $namespace = '';
-        $class = '';
         $tokens = token_get_all($content);
-        $count = count($tokens);
+        /** @var array<int, mixed> $tokens */
 
-        for ($i = 0; $i < $count; $i++) {
-            if (! isset($tokens[$i][0])) {
-                continue;
-            }
+        $namespace = self::extractNamespace($tokens);
+        $class = self::extractClassName($tokens);
 
-            if ($tokens[$i][0] === T_NAMESPACE) {
-                for ($j = $i + 1; $j < $count; $j++) {
-                    if ($tokens[$j][0] === T_NAME_QUALIFIED) {
-                        $namespace = $tokens[$j][1];
-                        break;
-                    }
-                }
-            }
-
-            if ($tokens[$i][0] !== T_CLASS && $tokens[$i][0] !== T_INTERFACE) {
-                continue;
-            }
-
-            for ($j = $i + 1; $j < $count; $j++) {
-                if ($tokens[$j][0] === T_STRING) {
-                    $class = $tokens[$j][1];
-                    break 2;
-                }
-            }
-        }
-
-        if ($class === '') {
+        if ($class === null) {
             return null;
         }
 
-        return $namespace ? $namespace . '\\' . $class : $class;
+        if ($namespace === null) {
+            return $class;
+        }
+
+        return $namespace . '\\' . $class;
+    }
+
+    /** @param array<int, mixed> $tokens*/
+    private static function extractNamespace(array $tokens): string|null
+    {
+        /** @psalm-suppress MixedAssignment */
+        foreach ($tokens as $index => $token) {
+            if (is_array($token) && $token[0] !== T_NAMESPACE) {
+                continue;
+            }
+
+            for ($j = $index + 1, $count = count($tokens); $j < $count; $j++) {
+                assert(is_array($tokens[$j]) && isset($tokens[$j][0]));
+                if ($tokens[$j][0] === T_NAME_QUALIFIED) {
+                    $string = $tokens[$j][1];
+                    assert(is_string($string));
+
+                    return $string;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /** @param array<int, mixed> $tokens */
+    private static function extractClassName(array $tokens): string|null
+    {
+        foreach ($tokens as $index => $token) {
+            assert(is_array($token));
+            if ($token[0] !== T_CLASS && $token[0] !== T_INTERFACE) {
+                continue;
+            }
+
+            for ($j = $index + 1, $count = count($tokens); $j < $count; $j++) {
+                if (is_array($tokens[$j]) && $tokens[$j][0] === T_STRING) {
+                    $string = $tokens[$j][1];
+                    assert(is_string($string));
+
+                    return $string;
+                }
+            }
+        }
+
+        return null;
     }
 }
