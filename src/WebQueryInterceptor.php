f949f55c97eb9abdb31c4669dd2a6bec50e3df95
Yoshitaka Jingu <jingu@bitnauts.co.jp>
2025-03-03 12:01:00 +0900

    Remove Pages return type from WebQueryInterceptor::invoke

--- src/WebQueryInterceptor.php
+++ src/WebQueryInterceptor.php
@@ -20,8 +20,8 @@ final class WebQueryInterceptor implements MethodInterceptor
     ) {
     }
 
-    /** @return Pages<mixed>|array<string, mixed>|string */
-    public function invoke(MethodInvocation $invocation): Pages|array|string
+    /** @return array<string, mixed>|string */
+    public function invoke(MethodInvocation $invocation): array|string
     {
         $method = $invocation->getMethod();
         /** @var WebQuery $webQuery */
