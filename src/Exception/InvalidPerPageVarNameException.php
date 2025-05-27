b5547e0470ecc722892e620238992efbfe6cbd1a
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-05-28 08:50:55 +0900

    Fix typo in InvalidPerPageVarNameException docblock
    Corrected a minor typo in the PHPDoc comment. This improves clarity and ensures the comment accurately reflects its intended meaning.

--- src/Exception/InvalidPerPageVarNameException.php
+++ src/Exception/InvalidPerPageVarNameException.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Ray\MediaQuery\Exception;
 
 /**
- * The argument name spfinal ecified in PerPage does not exist in the arguments of that method
+ * The argument name specified in PerPage does not exist in the arguments of that method
  */
 final class InvalidPerPageVarNameException extends LogicException
 {
