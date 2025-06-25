07544851e8ddfcc4939fa5198e90142509d888d5
Yuki Adachi <177159+yuki777@users.noreply.github.com>
2025-06-25 10:01:33 +0700

    Fix coding style and static analysis warnings

--- src/SqlQuery.php
+++ src/SqlQuery.php
@@ -154,7 +154,7 @@ final class SqlQuery implements SqlQueryInterface
             $sqls .= ';';
         }
 
-        $sqls = preg_replace('/^\s*--.*$/m', '', $sqls);
+        $sqls = (string) preg_replace('/^\s*--.*$/m', '', $sqls);
 
         $sqls = explode(';', trim($sqls, "\\ \t\n\r\0\x0B"));
         array_pop($sqls);
